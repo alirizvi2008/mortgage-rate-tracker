b34424615be874147e1710c4ad264623a3eb2b3f
arizvi <ali.rizvi@booking.com>
2026-02-21 19:46:58 +0100

    Remove whatsapp from dist

--- dist/notifications/whatsapp.d.ts
+++
@@ -1,8 +0,0 @@
-import { RateResult } from '../types';
-interface WhatsAppConfig {
-    phone: string;
-    apiKey: string;
-}
-export declare function sendWhatsApp(result: RateResult, config: WhatsAppConfig): Promise<void>;
-export {};
-//# sourceMappingURL=whatsapp.d.ts.map
